3c51763d87057bbf6598d36155db813810737d9c
Rob Dawson <robhdawson@gmail.com>
2016-11-17 20:58:27 -0500

    it works

---
+++ src/js/font.js
@@ -0,0 +1,30 @@
+module.exports = {
+    e: [
+        'XXXXX',
+        'X    ',
+        'XXX  ',
+        'X    ',
+        'XXXXX',
+    ],
+    h: [
+        'X   X',
+        'X   X',
+        'XXXXX',
+        'X   X',
+        'X   X',
+    ],
+    y: [
+        'X   X',
+        'X   X',
+        ' XXX ',
+        '  X  ',
+        '  X  ',
+    ],
+    '!': [
+        'X',
+        'X',
+        'X',
+        ' ',
+        'X',
+    ],
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,53 @@
 'use strict';
 
-console.log('Hiii~~');
+const $ = require('jquery');
+
+const font = require('./font');
+
+$(document).ready(function() {
+    const $input = $('[data-input]');
+    const $char = $('[data-char]');
+    const $filler = $('[data-filler]');
+
+    const $go = $('[data-go]');
+
+    const $output = $('[data-output]');
+
+    function values() {
+        return {
+            text: $input.val(),
+            char: $char.val() || 'X',
+            filler: $filler.val() || ' ',
+        };
+    }
+
+    function go() {
+        const stuff = values();
+
+        const rows = [[], [], [], [], []];
+
+        stuff.text.toLowerCase().split('').forEach(function(c) {
+            const bigBoy = font[c];
+
+            if (!bigBoy || bigBoy.length !== 5) {
+                throw `Hey! "${c}" is missing from the font, or it's invalid.`;
+            }
+
+            bigBoy.forEach(function(s, i) {
+                rows[i].push(s);
+            });
+        });
+
+        let output = rows.map(function(row) {
+            return row.join(' ');
+        }).join('\n');
+
+        output = output.replace(/X/g, stuff.char);
+        output = output.replace(/ /g, stuff.filler);
+
+        $output.text(output);
+        $output.focus().select();
+    }
+
+    $go.on('click', go);
+});
