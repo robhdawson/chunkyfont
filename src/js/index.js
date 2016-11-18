f721e1e2e5bad4000217c3c89bcb2ca0386bf6d7
Rob Dawson <robhdawson@gmail.com>
2016-11-17 22:51:34 -0500

    it works

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,24 @@
 'use strict';
 
 const $ = require('jquery');
+const _ = require('lodash');
 
 const font = require('./font');
+
+const starters = [
+    'hey!',
+    'frogs',
+    'conga',
+    'why',
+];
 
 $(document).ready(function() {
     const $input = $('[data-input]');
     const $char = $('[data-char]');
     const $filler = $('[data-filler]');
+
+    $char.val('X');
+    $filler.val(' ');
 
     const $go = $('[data-go]');
 
@@ -21,10 +32,10 @@ $(document).ready(function() {
         };
     }
 
-    function go() {
+    function go(noselect = false) {
         const stuff = values();
 
-        const rows = [[], [], [], [], []];
+        const letters = [];
 
         stuff.text.toLowerCase().split('').forEach(function(c) {
             const bigBoy = font[c];
@@ -33,21 +44,53 @@ $(document).ready(function() {
                 throw `Hey! "${c}" is missing from the font, or it's invalid.`;
             }
 
-            bigBoy.forEach(function(s, i) {
-                rows[i].push(s);
-            });
+            letters.push(bigBoy);
         });
 
-        let output = rows.map(function(row) {
-            return row.join(' ');
-        }).join('\n');
+        const layout = $('input[name="layout"]:checked').attr('value');
+        let output;
+
+        if (layout === 'vertical') {
+            const rows = [];
+            letters.forEach(function(letter, i) {
+                letter.forEach(function(row) {
+                    rows.push(row);
+                });
+
+                if (i < letters.length - 1) {
+                    rows.push('     ');
+                }
+            });
+
+            output = rows.join('\n');
+        } else {
+            const rows = [[], [], [], [], []];
+
+            letters.forEach(function(letter) {
+                letter.forEach(function(row, i) {
+                    rows[i].push(row);
+                });
+            });
+
+            output = rows.map(function(row) {
+                return row.join(' ');
+            }).join('\n');
+        }
 
         output = output.replace(/X/g, stuff.char);
         output = output.replace(/ /g, stuff.filler);
 
         $output.text(output);
-        $output.focus().select();
+
+        if (!noselect) {
+            $output.focus().select();
+        }
     }
 
-    $go.on('click', go);
+    $go.on('click', function() {
+        go();
+    });
+
+    $input.val(_.sample(starters));
+    go(true);
 });
